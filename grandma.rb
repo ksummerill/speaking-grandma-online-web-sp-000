5494695818d6bdb82980e9e7654db20362db3e00
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-15 02:09:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
@@ -11,15 +11,4 @@ def speak_to_grandma(phrase)
 end
 
 
-#
-# def speak_to_grandma(phrase)
-#   if phrase.downcase
-#     return "HUH?! SPEAK UP, SONNY!"
-#   elsif phrase.upcase
-#     return "NO, NOT SINCE 1938!"
-#   else phrase == "I LOVE YOU GRANDMA!"
-#     return "I LOVE YOU TOO PUMPKIN!"
-#
-#   end
-# # speak_to_grandma(phrase)
-# end
+
