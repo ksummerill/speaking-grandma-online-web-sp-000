c3fe46a16451b90937b86ad12119df6b66b31c6c
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-15 01:55:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  # if phrase.upcase
-  #   return "HUH?! SPEAK UP, SONNY!"
+  if phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
