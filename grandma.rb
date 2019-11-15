125e5818acfb979d8e7f38ab73dcf593d4d71a3a
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-15 02:04:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-  else phrase.downcase
+  else phrase.capitalize
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
